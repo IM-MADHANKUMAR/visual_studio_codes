58deaa371fe00482a300fdcc507ae101c6e22483
IM-MADHANKUMAR <88707895+IM-MADHANKUMAR@users.noreply.github.com>
2022-07-08 12:49:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const num1=parseFloat(prompt("enter 1st number :"));
+const num2=parseFloat(prompt("enter 2nd number :"));
+const num3=parseFloat(prompt("enter 3rd number :"));
+const largest = Math.max(num1,num2,num3);
+alert("the largest number is ",largest);
